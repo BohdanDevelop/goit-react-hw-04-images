5421de2b2252f5c55f8e42a83fc2d2e12f842edc
BohdanDeveloper <radchukyou1999@gmail.com>
2022-05-28 14:49:48 +0300

    removed propTypes from gallery

--- src/components/SearchingImages/components/ImageGallery/ImageGallery.jsx
+++ src/components/SearchingImages/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,4 @@
 import style from './ImageGallery.module.scss';
-import PropTypes from 'prop-types';
 
 const ImageGallery = ({ children }) => {
   return <ul className={style['ImageGallery']}>{children}</ul>;
