bfc470a1e639c3ca02ec3267c70031687842a41f
BohdanDeveloper <radchukyou1999@gmail.com>
2022-06-04 00:21:07 +0300

    ss

--- src/components/SearchingImages/components/Modal/Modal.jsx
+++ src/components/SearchingImages/components/Modal/Modal.jsx
@@ -1,9 +1,20 @@
 import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
 import style from './Modal.module.scss';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.getElementById('modal-root');
 const Modal = ({ image, alt, closeModal }) => {
+  useEffect(() => {
+    const escModal = evt => {
+      if (evt.code === 'Escape') closeModal();
+    };
+    document.addEventListener('keydown', escModal);
+    return () => {
+      document.removeEventListener('keydown', escModal);
+      console.log('removed');
+    };
+  }, []);
   return createPortal(
     <div className={style.Overlay}>
       <div className={style.Modal}>
